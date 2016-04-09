9cda70c69e71009a0b2af2d4545e9bd04b89e26c
Jason Tyler <jmtyler@gmail.com>
2016-04-09 00:23:05 -0700

    More reorganization. Added set_from_tunes() and get_tunes()

--- classes.py
+++ classes.py
@@ -1,16 +1,20 @@
 import sys
+import json
 from random import sample
 from collections import namedtuple, deque
 
 
+tune_files = ['data/reels.json']
 tunetypes = set(["Reel", "Jig", "Hornpipe", "Polka", "Barndance"])
 source_codes = set(["TC", "AL", "RB"])
 musical_keys = set(["Dmaj", "Gmaj", "Emin"])
 musical_modes = set([])
+catalogue = dict()
+
 
 
 class Tune(object):
-    def __init__(self, name, key=None, tunetype=None, played=None, next_in_set=None, source_code=None, heard=None, mode=None):
+    def __init__(self, name, key=None, tunetype=None, played=None, next_in_set=None, source_code=None, heard=None, mode=None, abc=None):
         """Define the Tune with all reasonable parameters
 
         str:            name --> human readable name; will be used as a hash
@@ -21,6 +25,7 @@ class Tune(object):
         str:            tunetype --> type of tune, i.e. jig or reel etc
         list[str]:      next_in_set --> list of name(s) for tune that reasonably follows playing this one; used to construct on the fly sets
         str:            source_code --> code for where tune is from; use TC for 'tune class', AL for 'album', RB for 'from Randal Bays lesson'
+        str:            abc --> String for ABC notation
         """
 
         self.name = name
@@ -31,6 +36,43 @@ class Tune(object):
         self.tunetype = tunetype if tunetype in tunetypes else None
         self.next_in_set = list() if not next_in_set else next_in_set
         self.source_code = source_code if source_code in source_codes else None
+        self.abc = abc
 
 
+def load_tunes(tune_files):
+    tunes = dict()
+    for filename in tune_files:
+        with open(filename) as file:
+            serialized_tunes = json.load(file)
+
+        for _, tune_dict in serialized_tunes.items():
+            new_tune = Tune(**tune_dict)
+            tunes[tune_dict['name']] = new_tune
+    return tunes
+
+
+new_tunes = load_tunes(tune_files)
+catalogue.update(new_tunes)
+
+
+def get_tune(substr, catalogue=catalogue):
+    matches = [tune for tune_name, tune in catalogue.items() if substr.lower() in tune_name.lower()]
+    if matches:
+        return sample(matches, 1)[0]
+    else:
+        return None
+
+
+def set_from_tune(tune, nmax=3):
+    tune_set = [tune]
+    while len(tune_set) < nmax:
+        try:
+            next_tune = sample(tune.next_in_set, 1)
+        except ValueError: # No other tune
+            return tune_set if len(tune_set) > 1 else None
+        else:
+            get_tune()
+            tune_set.append(next_tune)
+            tune = next_tune
+    return tune_set
 
--- engine.py
+++ engine.py
@@ -1,12 +1,3 @@
-import json
-from classes import Tune, Set
 
 
-with open('reels.json') as file:
-    reels_serialized = json.load(file)
 
-reels = dict()
-
-for _, tune_dict in reels_serialized.items():
-    new_tune = Tune(**tune_dict)
-    reels[tune_dict['name']] = new_tune
