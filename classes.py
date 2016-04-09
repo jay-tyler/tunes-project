67d4c0648621827c859d3a82e8ffe3730a84f0ec
Jason Tyler <jmtyler@gmail.com>
2016-04-08 22:58:24 -0700

    Removed unnecessary Set() class; dir restructure

--- classes.py
+++ classes.py
@@ -9,14 +9,6 @@ musical_keys = set(["Dmaj", "Gmaj", "Emin"])
 musical_modes = set([])
 
 
-class Node(object):
-    """Object for tree searching"""
-    def __init__(self, distance=sys.maxint, parent=None, reference=None):
-        self.distance = distance
-        self.parent = parent
-        self.reference = reference
-
-
 class Tune(object):
     def __init__(self, name, key=None, tunetype=None, played=None, next_in_set=None, source_code=None, heard=None, mode=None):
         """Define the Tune with all reasonable parameters
@@ -41,71 +33,4 @@ class Tune(object):
         self.source_code = source_code if source_code in source_codes else None
 
 
-class Set(object):
 
-
-    @staticmethod
-    def set_gen(start_tune, catalog, target_length=3, _preceeding=None):
-        """Return a generator containing Tunes, regardless of whether they belong to a current Set object.
-
-        Tunes are randomly chosen by next_in_set reference.
-        Number of tunes will be equal to or less than target_length.
-
-        hash: start_tune --> hash to Tune object that begins the set
-        dict: catalog --> a dict of Tune objects to select from
-        int:  target_length --> set will be <= target_length
-
-        Not yet fully implemented:
-        Tune: _preceeding --> Tune object preceeding in set
-
-        """
-        if not _preceeding:
-            _preceeding = catalog[start_tune]
-        for i in range(target_length):
-            yield _preceeding
-            try:
-                next_hash = sample(_preceeding.next_in_set, 1)[0]
-                _preceeding = catalog[next_hash]
-            except ValueError:
-                raise StopIteration
-
-    @staticmethod
-    def gen_all_sets(start_tune, catalog):
-        """Return a list of all sets from start_tune hash; inner lists consist of Tune hashes corresponding to a set.
-
-        This method returns all potential sets regardless of whether they exist as Set objects currently.
-
-        Currently implemented as using 'next_in_set' node edges
-
-        Implemented after non-recursive approach psuedocode from
-        https://en.wikipedia.org/wiki/Breadth-first_search
-        """
-        #  Set up records for non-recursive breadth first search
-        records = {tune.name: Node(distance=sys.maxint, parent=None, reference=tune)
-            for hast, tune in catalog.iteritems()}
-
-        #  grab start_tune record and initialize to queue
-        records[start_tune] = Node(distance=0, parent=None, reference=catalog[start_tune])
-        to_travel = deque([records[start_tune]])
-
-        #  ordered structure to place processed tune references
-        travelled = []
-
-        # TODO: add check for cycles; i.e. if a reference points to the start_record, terminate it
-        while to_travel:
-            current = to_travel.popleft()
-            for tune in current.reference.next_in_set:
-                tune = records[tune]
-                if tune.distance == sys.maxint:
-                    tune.distance = current.distance + 1
-                    tune.parent = current
-                    to_travel.append(tune)
-            travelled.append(current)
-
-        return travelled
-        # # TODO: splay out tunes into lists in lists
-        # to_return = []
-        # for tune in travelled:
-        #     if not tune.self.next_in_set:
-        #         # In this case, tune is at the end of a set
-        #         pass
